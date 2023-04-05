2b2a23385feba11b9caf505658fc9afd4962b474
Ana Matei <anamatei.099@gmail.com>
2023-04-06 00:37:36 +0200

    Added logic for state changes

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Accordion({ items }) {
-  const renderedItems = items.map((item) => {
+  const [expandedIndex, setExpandedIndex] = useState(1);
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    const content = isExpanded && <div>{item.content}</div>;
+
+    //&& operator gives back the first falsey value or the last truthy value
+    //|| operator gives back the first value that is truthy
+    //React doesn't print booleans, nulls or undefined. So if isExpanded is false it won't show the div
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {content}
+        {/* this is the same withut content variable
+        {isExpanded && <div>{item.content}</div>} */}
       </div>
     );
   });
