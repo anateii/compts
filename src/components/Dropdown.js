1612cd5766dc207a40466b715eeb691f1222aaae
Ana Matei <anamatei.099@gmail.com>
2023-05-23 16:12:58 +0200

    Dropdown toggle and select events

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
 function Dropdown({ options }) {
-  return <div>Dropdown</div>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleTogleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (option) => {
+    //CLOSE DROPDOWN WHEN OPTION IS SELECTED
+    setIsOpen(false);
+    //WHAT OPTION DID THE USER CLICK ON?
+    console.log(option);
+  };
+
+  const renderedOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleSelect(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div onClick={handleTogleDropdown}>Select...</div>
+      {isOpen && <div>{renderedOptions}</div>}
+    </div>
+  );
 }
 
 export default Dropdown;
