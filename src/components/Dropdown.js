b5696c9c43f8916db9933e836b9e79b85b5a87e3
Ana Matei <anamatei.099@gmail.com>
2023-06-01 11:00:06 +0200

    Styling dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { GoChevronDown } from "react-icons/go";
 
 function Dropdown({ options, onSelect, selected }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,7 +18,11 @@ function Dropdown({ options, onSelect, selected }) {
 
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleSelect(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleSelect(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
@@ -29,15 +34,23 @@ function Dropdown({ options, onSelect, selected }) {
   //   }
 
   return (
-    <div>
-      <div onClick={handleTogleDropdown}>
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-3 shadow bg-white w-full"
+        onClick={handleTogleDropdown}
+      >
         {" "}
         {/*{content}*/}
         {selected?.label || "Select..."}
+        <GoChevronDown className="text-lg" />
         {/* The question mark checks if selected is null, if it is then the or operator gives
         back the first truthy value in this case the Select string */}
       </div>
-      {isOpen && <div>{renderedOptions}</div>}
+      {isOpen && (
+        <div className="absolute top-full border rounded p-3 shadow bg-white w-full">
+          {renderedOptions}
+        </div>
+      )}
     </div>
   );
 }
